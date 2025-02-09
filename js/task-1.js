5218552b85ae42c358f57b5d227f24c0f5229f70
Ekaterina <katedubinets22@gmail.com>
2025-02-09 22:09:09 +0200

    1task

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,14 @@
-function slugify(title) {
-    const slugifyNew = title.toLowerCase().split(" ");
-    const slug = slugifyNew.join("-");
-    return slug
+// function slugify(title) {
+//     const slugifyNew = title.toLowerCase().split(" ");
+//     const slug = slugifyNew.join("-");
+//     return slug
+// }
+
+function slugify(title){
+    return title.toLowerCase().replaceAll(" ","-");
 }
 
 console.log(slugify("Arrays for beginners"));
-console.log(slugify("English for developer")); 
+console.log(slugify("English for developer"));
 console.log(slugify("Ten secrets of JavaScript")); 
 console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); 
